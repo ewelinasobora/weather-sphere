0af0205a2163f522bfefaa926bbe553530f371fd
Ewelina Sobora <e@e-dot.uk>
2023-11-21 16:11:57 +0000

    feat: retrieve weather data based on city input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,28 @@
- // This function handles events where one button is clicked
+const searchInput = $("#search-input");
+// function handles events where one button is clicked
  $("#search-button").on("click", function(event) {
   event.preventDefault();
+   // it grabs the input from the textbox
+   const cityName = searchInput.val().trim();
 
-  var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=33.4&lon=-94.4&appid=3daaa686e6b7ee2c9173bb0fd254fbba";
+   // it stores the input in local storage
+   localStorage.setItem("cityName", cityName);
+   getWeatherBy(cityName)
+
+ });
+
+function getWeatherBy(cityName) {
+
+  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=20e460d7e4a79fa6b27760389b12e22f`;
 
   fetch(apiUrl)
-  .then(function (response) {
+    .then(function (response) {
     // returning the json data from the url
-    return response.json();
-  })
+      return response.json();
+    })
+    // executes function after we get the data
+    .then(function (data) {
 
-  // executes function after we get the data
-  .then(function (data) {
-    console.log(data);
-  })
- });
+      console.log(data);
+    })
+}
